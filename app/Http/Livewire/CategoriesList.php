a4eb9b6b5341f4763c696b938cd3af73ef4a907f
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-04-30 00:25:02 +0500

    Categories Table - Toggle Active

--- app/Http/Livewire/CategoriesList.php
+++ app/Http/Livewire/CategoriesList.php
@@ -14,6 +14,7 @@ class CategoriesList extends Component
     public Category $category; 
  
     public bool $showModal = false;
+    public array $active = [];
 
     public function openModal() 
     {
@@ -42,12 +43,23 @@ class CategoriesList extends Component
         $this->category->save();
  
         $this->reset('showModal');
-    } 
+    }
+
+    public function toggleIsActive($categoryId) 
+    {
+        Category::where('id', $categoryId)->update([
+            'is_active' => $this->active[$categoryId],
+        ]);
+    }
  
     public function render()
     {
-        $categories = Category::paginate(10); 
- 
+        $categories = Category::paginate(10);
+
+        $this->active = $categories->mapWithKeys( 
+            fn (Category $item) => [$item['id'] => (bool) $item['is_active']]
+        )->toArray();
+
         return view('livewire.categories-list', [
             'categories' => $categories, 
         ]);
